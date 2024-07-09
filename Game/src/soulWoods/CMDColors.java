892ba892d94077311c9c5a6f6dd6bdb64344f90e
Casbian <kai.erichreineke@gmail.com>
2024-07-09 23:46:17 +0200

    Add files via upload (#1)

---
+++ Game/src/soulWoods/CMDColors.java
@@ -0,0 +1,13 @@
+//Package
+package soulWoods;
+//Ansi Codes in use to color CMD/Console Output
+//CMDColors Class
+class CMDColors {
+final String RESET = "\u001B[0m";			//Resets String Color
+final String RED = "\u001B[91m";			//Rot
+final String GREEN = "\u001B[92m";			//Gruen
+final String YELLOW = "\u001B[93m";			//Gelb
+final String PURPLE = "\u001B[95m";			//Lila
+final String CYAN = "\u001B[96m";			//Cyan
+final String CONSOLE = "\033[H\033[2J";		//Clear Console or CMD
+}
---
+++ Game/src/soulWoods/Monster.java
@@ -0,0 +1,10 @@
+//Package
+package soulWoods;
+//Monster Class
+class Monster {
+//Variables or Attributes in use	
+int monsterLevel = 0;
+int monsterHealth = 0;
+int monsterSpeed = 0;
+int monsterDamage = 0;
+}
