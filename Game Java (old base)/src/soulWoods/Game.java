d5e9f9069f88428da8309d67ea07e5f47b06a05e
Casbian <kai.erichreineke@gmail.com>
2024-07-10 23:32:19 +0200

    Old Java Version
    Base Version that gave me the Idea

---
+++ Game Java (old base)/src/soulWoods/Game.java
@@ -0,0 +1,21 @@
+//Package
+package soulWoods;
+//Game Class
+public class Game {
+//Format Object
+CMDColors c = new CMDColors();
+//Variables or Attributes in use
+String welcomeArt = ""
++"\n███████  ██████  ██    ██ ██      ██     ██  ██████   ██████  ██████  ███████"
++"\n██      ██    ██ ██    ██ ██      ██     ██ ██    ██ ██    ██ ██   ██ ██     "
++"\n███████ ██    ██ ██    ██ ██      ██  █  ██ ██    ██ ██    ██ ██   ██ ███████"
++"\n     ██ ██    ██ ██    ██ ██      ██ ███ ██ ██    ██ ██    ██ ██   ██      ██"
++"\n███████  ██████   ██████  ███████  ███ ███   ██████   ██████  ██████  ███████";
+String welcomeText = ""
++"\nWillkommen in den SoulWoods!"
++"\nBereite dich auf ein episches Abenteuer vor, tapferer Held."
++"\nTief im dichteren Wald lauern unzählige Monster, auf der Suche nach Seelen."
++"\nDeine Mission: Besiege sie alle bis die Welt zerfällt."
++"\nMach dich bereit für die Herausforderung deines Lebens!"
++"\n";
+}
---
+++ Game Java (old base)/src/soulWoods/Upgrades.java
@@ -0,0 +1,88 @@
+//Package
+package soulWoods;
+//Upgrades Class
+class Upgrades {
+//Format Object
+CMDColors c = new CMDColors();
+//Variables or Attributes in use
+int heilung = (int)(Math.random() * 10)+1;
+int damageBuff = (int)(Math.random() * 20)+1;
+int speedBuff = (int)(Math.random() * 2)+1;
+String upgradesFrame = ""
++"\nLevel UP"
++"\nCurrent Stats"
++"\n -- HP -<            %d"
++"\n -- Speed -<         %d"
++"\n -- Damage -<        %d"
++"\n -- Flee Counter -<  %d"
++"\nCHOOSE"
++"\n-- Heal   --  %d"
++"\n-- Speed  --  %d"
++"\n-- Damage --  %d";
+String[] itemsBuff = {"EnergieGetraenk", "Kippe", "Admin Passwort", "Beschwerde"};
+String[] itemsDebuff = {"Hodenhalter","SUS Pössel","The Man Himself","Alter Mann"};
+//Methods
+String formatupgradesFrame(String Frame,Player y) {
+String resultFrame = String.format(Frame,y.playerHealth,y.playerSpeed,y.playerDamage,y.playerFleeCounter,heilung,speedBuff,damageBuff);
+return resultFrame;
+}
+int chooseHeal(Player y) {
+y.playerHealth = y.playerHealth+heilung;
+return y.playerHealth;
+}
+int chooseSpeed(Player y) {
+y.playerSpeed = y.playerSpeed+speedBuff;
+return y.playerSpeed;
+}
+int chooseDamage(Player y) {
+y.playerDamage = y.playerDamage+damageBuff;	
+return y.playerDamage;
+}
+String randomItem(String[] itemDebuff,String[] itemBuff) {
+int randomNumber1 = (int)(Math.random() * 2);
+String result = null;
+switch (randomNumber1) {
+case 0:
+int randomNumber2 = (int)(Math.random() * itemBuff.length);
+result = itemBuff[randomNumber2];
+break;
+case 1:
+randomNumber2 = (int)(Math.random() * itemDebuff.length);
+result = itemDebuff[randomNumber2];
+break;
+}
+return result;
+}
+int energieGetraenk(Player y) {
+y.playerSpeed = y.playerSpeed+10;
+return y.playerSpeed;
+}
+int kippe(Player y) {
+y.playerDamage = y.playerDamage+10;
+return y.playerDamage;
+}
+int adminPasswort(Player y) {
+y.playerHealth = y.playerHealth+50;
+return y.playerHealth;
+}
+int beschwerde(Player y) {
+y.playerDamage = y.playerDamage+30;
+return y.playerDamage;
+}
+int hodenhalter(Player y) {
+y.playerSpeed = y.playerSpeed-10;
+return y.playerSpeed;
+}
+int sUSPössel(Player y) {
+y.playerDamage = y.playerDamage-20;
+return y.playerDamage;
+}
+int theManHimself(Player y) {
+y.playerHealth = y.playerHealth-30;
+return y.playerHealth;
+}
+int alterMann(Player y) {
+y.playerSpeed = y.playerSpeed-25;
+return y.playerSpeed;
+}
+}
