5c312faf4e7e9feec931a947fc2d9b466f0d1ada
Casbian <kai.erichreineke@gmail.com>
2024-07-15 00:40:53 +0200

    Delete SoulWoods directory

--- SoulWoods/Monster.py
+++
@@ -1,8 +0,0 @@
-import random
-class Monster:
-    def __init__(self, ROUNDCOUNTER):
-        self.instance = ROUNDCOUNTER
-        self.monsterLevel = int (0)
-        self.monsterHP = int (random.uniform(1,1000)+self.instance)
-        self.monsterSpeed = int (random.uniform(1,100)+self.instance)
-        self.monsterDamage = int (random.uniform(1,50)+self.instance)
--- SoulWoods/Upgrade.py
+++
@@ -1,7 +0,0 @@
-import random
-class UpgradeClass:
-    def __init__(self, ROUNDCOUNTER):
-        self.instance = ROUNDCOUNTER
-        self.heilung = int (random.uniform(1,100)+ROUNDCOUNTER)
-        self.damageBuff = int (random.uniform(1,20)+ROUNDCOUNTER)
-        self.speedBuff = int (random.uniform(1,20)+ROUNDCOUNTER)
