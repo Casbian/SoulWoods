e46e7888db60bef0f2ceaceac39d6a9d24ae213c
Casbian <kai.erichreineke@gmail.com>
2024-07-25 19:10:58 +0200

    Delete engine directory

--- engine/SOULWOODS.py
+++
@@ -1,7 +0,0 @@
-import customtkinter as ctk
-import tkinter as tk
-import random
-class SOULWOODS():
-    def __init__(self):
-        self.TITLEART = tk.PhotoImage(file="bin/assets/GameEssential/SoulWoodsTitle.png")
-        self.TITLEARTSCALED = self.TITLEART.subsample(2,2)
